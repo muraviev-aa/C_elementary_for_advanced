13c167e369af37e6e092f588c216b9eab4542576
Aleksei Muraviev <muraviev-aa@yandex.ru>
2023-11-22 19:57:05 +0300

    Added 2

---
+++ 2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[7] = "Hello!";
+    printf("%s\n", str);
+
+    char str1[7] = {'H', 'e', 'l', 'l', 'o', '!'};
+    printf("%s", str1);
+
+    return 0;
+}
