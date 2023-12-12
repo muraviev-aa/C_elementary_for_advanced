8a92d6a231c09048f670f5a6744b34fb98ce37c9
Aleksei Muraviev <muraviev-aa@yandex.ru>
2023-12-12 21:29:58 +0300

    Added 44, 45

---
+++ files/44.c
@@ -0,0 +1,23 @@
+// 44. Пердача массива в функцию №2
+#include <stdio.h>
+
+/*
+В функцию передается не сам массив, как единый набор чисел,
+а только адрес первого элемента массива и отдельно общее
+количество элементов
+*/
+
+void printArray(int * array, size_t num)
+{
+    unsigned ctr;
+    for(ctr=0; ctr<num; ctr++)
+        printf("%d ", *(array+ctr));
+}
+
+int main()
+{
+    int money[] = {-12, 22, -9, 8, 0, 10};
+    printArray(money, sizeof(money)/sizeof(money[0]));
+
+    return 0;
+}
---
+++ files/45.c
@@ -0,0 +1,23 @@
+// 45. Пердача массива в функцию №3
+
+#include <stdio.h>
+#define SIZEARR(x) (sizeof(x) / sizeof(x[0]))
+
+int money[] = {100, -2, 56};
+
+void printArray(int * array, size_t num)
+{
+    unsigned ctr;
+    for(ctr=0; ctr<num; ctr++)
+        printf("%d ", *(array+ctr));
+}
+
+int main()
+{
+    int account[] = {100, 200, 300, 400, 500};
+    printArray(money, SIZEARR(money));
+    putchar('\n');
+    printArray(account, SIZEARR(account));
+
+    return 0;
+}
