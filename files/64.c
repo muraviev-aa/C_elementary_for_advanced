5d6fd84bd07e251561a640f6a4137ea999ea255d
Aleksei Muraviev <muraviev-aa@yandex.ru>
2023-12-29 18:41:36 +0300

    Update 64.c

--- files/64.c
+++ files/64.c
@@ -5,22 +5,29 @@
 #include <stdio.h>
 #include <windows.h>
 
-void printFormat(DWORD millTime)
-{
-    DWORD day, hour, min, sec, millsec;
-    millsec = millTime%1000;
-    sec = (millTime/1000)%60;
-    min = ((millTime/1000)/60)%60;
-    hour = (((millTime/1000)/60)/60)%24;
-    day =(millTime/1000)/(60*60*24);
-    printf("%lu days, %lu hour, %lu min, %lu sec, %lu millsec\n", day, hour, min, sec, millsec);
-}
+void timeStart(DWORD millTime);
 
 int main()
 {
     DWORD myTime = 0;
     myTime = GetTickCount();
     printf("%lu since the system was started\n", myTime);
-    printFormat(myTime);
+    timeStart(myTime);
     return 0;
 }
+
+void timeStart(DWORD millTime)
+{
+    DWORD day, hour, min, sec, millsec, result;
+    result = millTime;
+    millsec = result%1000;
+    result = result/1000;
+    sec = result%60;
+    result = result/60;
+    min = result%60;
+    result = result/60;
+    hour = result%24;
+    day = result/24;
+    printf("%lu day, %lu hour, %lu min, %lu sec, %lu millsec", day, hour, min, sec, millsec);
+}
+
